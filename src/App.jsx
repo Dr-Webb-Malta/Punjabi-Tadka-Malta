26268210ecda2d87622ca4dbd9845aed8575b22b
Mitul Rana <miittulrana@gmail.com>
2025-01-03 02:45:46 +0100

    new dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css';
 import './styles/components.css';
 import './styles/animations.css';
